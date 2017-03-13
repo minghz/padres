2625f78209a1c1a5813f8b514851e81e840b6fbb
Mel Zhao <mel.zhao@globacore.com>
2017-03-13 19:10:26 -0400

    import graphstream and try tutorial

--- src/main/java/ca/utoronto/msrg/padres/daemon/Daemon.java
+++ src/main/java/ca/utoronto/msrg/padres/daemon/Daemon.java
@@ -1,8 +1,22 @@
 package ca.utoronto.msrg.padres.daemon;
 
+import org.graphstream.graph.*
+import org.graphstream.graph.implementations.*
+
 public class Daemon
 {
    public static void main(String[] args){
-	   System.out.println("Hello World");
+	   //System.out.println("Hello World");
+	   
+	   Graph graph = new SingleGraph("Tutorial 1");
+	   
+	   graph.addNode("A" );
+	   graph.addNode("B" );
+	   graph.addNode("C" );
+	   graph.addEdge("AB", "A", "B");
+	   graph.addEdge("BC", "B", "C");
+	   graph.addEdge("CA", "C", "A");
+	   
+	   graph.display();
    }
 }
