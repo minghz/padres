8eff710064ff975d7ccf19770dd20b106e25569b
Mel Zhao <mel.zhao@globacore.com>
2017-03-13 20:37:36 -0400

    fix missing smicolon and add more func

--- src/main/java/ca/utoronto/msrg/padres/daemon/Daemon.java
+++ src/main/java/ca/utoronto/msrg/padres/daemon/Daemon.java
@@ -1,12 +1,13 @@
 package ca.utoronto.msrg.padres.daemon;
 
-import org.graphstream.graph.*
-import org.graphstream.graph.implementations.*
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
 
 public class Daemon
 {
    public static void main(String[] args){
-	   //System.out.println("Hello World");
+	   System.out.println("Hello World");
+	   //System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 	   
 	   Graph graph = new SingleGraph("Tutorial 1");
 	   
@@ -18,5 +19,13 @@ public class Daemon
 	   graph.addEdge("CA", "C", "A");
 	   
 	   graph.display();
+	   
+	   for(Node n:graph.getEachNode()) {
+		   System.out.println(n.getId());
+	    }
+	   
+	    for(Edge e:graph.getEachEdge()) {
+	        System.out.println(e.getId());
+	    }
    }
 }
