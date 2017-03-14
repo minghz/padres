b4f9f7817c5c020091e67a95190e545483fbb10b
Mel Zhao <mel.zhao@globacore.com>
2017-03-13 22:44:33 -0400

    simple graph demo

--- src/main/java/ca/utoronto/msrg/padres/daemon/Daemon.java
+++ src/main/java/ca/utoronto/msrg/padres/daemon/Daemon.java
@@ -5,27 +5,16 @@ import org.graphstream.graph.implementations.*;
 
 public class Daemon
 {
-   public static void main(String[] args){
-	   System.out.println("Hello World");
-	   //System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
-	   
-	   Graph graph = new SingleGraph("Tutorial 1");
-	   
-	   graph.addNode("A" );
-	   graph.addNode("B" );
-	   graph.addNode("C" );
-	   graph.addEdge("AB", "A", "B");
-	   graph.addEdge("BC", "B", "C");
-	   graph.addEdge("CA", "C", "A");
-	   
-	   graph.display();
-	   
-	   for(Node n:graph.getEachNode()) {
-		   System.out.println(n.getId());
-	    }
-	   
-	    for(Edge e:graph.getEachEdge()) {
-	        System.out.println(e.getId());
-	    }
-   }
+		public static void main(String args[]) {
+			Graph graph = new SingleGraph("Tutorial 1");
+
+			graph.addNode("Apples");
+			graph.addNode("Bees");
+			graph.addNode("Cu");
+			graph.addEdge("ApplesBees", "Apples", "Bees");
+			graph.addEdge("BeesCu", "Bees", "Cu");
+			graph.addEdge("CuApples", "Cu", "Apples");
+
+			graph.display();
+		}
 }
