50597ac2d5a826f693199abd5200961bcf9acacb
Yale1123 <choco1123yale@gmail.com>
2017-03-13 20:39:01 -0400

    ZKCreate buggy code initial commit

---
+++ src/main/java/ca/utoronto/msrg/padres/ZKCreate/ZKExists.java
@@ -0,0 +1,53 @@
+package ca.utoronto.msrg.padres.ZKCreate;
+import java.io.IOException;
+
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooKeeperMain;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.data.Stat;
+
+
+
+
+
+public class ZKExists {
+   private static ZooKeeper zk;
+   private static ZooKeeperMain conn;
+
+   // Method to check existence of znode and its status, if znode is available.
+   public static Stat znode_exists(String path) throws
+      KeeperException,InterruptedException {
+      return zk.exists(path, true);
+   }
+   /*
+   public static void main(String[] args){
+    System.out.println("Hello World");
+   }*/
+/*
+   public static void main(String[] args) throws InterruptedException,KeeperException {
+      
+	   
+	   
+	   String path = "/MyFirstZnode"; // Assign znode to the specified path
+			
+      try {
+    	  
+         conn = new ZooKeeperMain(zk);
+        // zk = conn.connectToZK("localhost");
+         Stat stat = znode_exists(path); // Stat checks the path of the znode
+				
+         if(stat != null) {
+            System.out.println("Node exists and the node version is " +
+            stat.getVersion());
+         } else {
+            System.out.println("Node does not exists");
+         }
+				
+      } catch(Exception e) {
+         System.out.println(e.getMessage()); // Catches error messages
+      }
+   }*/
+}
