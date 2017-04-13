476a1d19f4c62995cb2977c16efc566ba29417cd
Yale1123 <choco1123yale@gmail.com>
2017-04-13 13:07:58 -0400

    daemonprocess

---
+++ src/main/java/ca/utoronto/msrg/padres/zkOperations/DaemonProcess.java
@@ -0,0 +1,296 @@
+package ca.utoronto.msrg.padres.daemon;
+
+import java.util.Iterator;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.zookeeper.AsyncCallback;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.Watcher.Event;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.apache.log4j.BasicConfigurator;
+
+
+public class DaemonProcess implements Watcher/*, AsyncCallback.StatCallback*/{
+	
+	 	ZooKeeper zk;
+	    ZooKeeperConnection conn;
+	    MainGraph MG;
+	
+	    public DaemonProcess(){
+	    	
+	       MG = new MainGraph();
+	    	
+	 	   try{
+	 	   conn = new ZooKeeperConnection();
+	        zk = new ZooKeeper("localhost", 5000,this);   
+	        zk = conn.connect("localhost");
+	 	   }catch (Exception e) {
+	 	         System.out.println("message"+e.getMessage()); //Catch error message
+	 	   }
+	 	   
+	 	  
+	    }
+	    
+	    static boolean flagDelete=false;
+	    
+	    public void process(WatchedEvent event) {
+	    	
+	    	
+	        String path = event.getPath();
+	        
+	        System.out.println();
+	        System.out.println("Current Path:"+path);
+	     //   System.out.println(event.getType().toString());
+	     //   System.out.println();
+	        
+	        
+	        String rootPath="/";
+	        
+	        
+	        
+	        if (event.getType() == Event.EventType.None) {
+	      //  System.out.println("None Event");
+	        	
+	            switch (event.getState()) {
+	                case SyncConnected:
+	                    break;
+	                case Expired:
+	                    break;
+	            }
+	            
+	            try{
+	            	
+	            	List<String> childrenList = new ArrayList<String>();
+	            	if(path!=null)
+	            	childrenList=zk.getChildren(path,this);
+	            	else
+	            	childrenList=zk.getChildren(rootPath,this);
+	            	
+	            	int i=0;
+	            	
+	            	for(Iterator iterator=childrenList.iterator();iterator.hasNext();){	 
+	            		i++;
+	            		String str=iterator.next().toString();
+	            		System.out.println("i="+i+" Children:"+str);
+	            		if(path==null){
+	            			System.out.println("/"+str);
+		            		zk.getChildren("/"+str, this);
+		            		break;
+		            	}
+		            	else if(path.equals("/")){
+		            		System.out.println(path+str);
+		            		zk.getChildren(path+str, this);
+		            	}
+		            	else{
+		            		System.out.println(path+"/"+str);
+		            		zk.getChildren(path+"/"+str, this);	
+		            	}
+	            	}
+	            	
+	           
+	            	if(!path.isEmpty()){
+	            		zk.getChildren(path, this);
+	            	}
+	                zk.getChildren(rootPath,this);
+	                
+	               
+	            }catch(Exception e){
+	                	System.out.println(e.getMessage());
+	                	
+	            }    
+	        } 
+	        
+	        
+	        else if (event.getType() == Event.EventType.NodeChildrenChanged){
+	        	
+	       	
+	        	if(flagDelete==false){
+	        		
+	        		//step 1: ZKget
+	        		
+	        		//step 2: get the whole graph get_graph()
+	        		
+	        		//ZKCreate("")
+	        		
+	        		
+	        		//Codes if Children is created not deleted
+				    // System.out.println("Node Children Changed Event");
+				        
+				        try{
+				        	
+				        List<String> childrenList = new ArrayList<String>();
+				        
+				        
+				        if(path!=null)
+			            	childrenList=zk.getChildren(path,this);
+			            else
+			            	childrenList=zk.getChildren(rootPath,this);
+				        
+				        
+				        int i=0;
+		            	
+				        String str;
+				        
+		            	for(Iterator iterator=childrenList.iterator();iterator.hasNext();i++){	 
+		            		
+		            		
+		            		str=iterator.next().toString();
+		            		
+		            		
+		            	//	System.out.println("i="+i+" Children:"+str);
+		            		if(path==null){
+		            			System.out.println(rootPath+str);
+			            		zk.getChildren(rootPath,this);
+			            		break;
+			            	}
+			            	else{
+			            		
+			            		
+			            		if(path.equals("/")){
+				            		System.out.println(path+str);
+				            		zk.getChildren(path+str, this);
+				            		
+				            		
+				            		
+				            	}
+				            	else{
+				            		System.out.println(path+"/"+str);
+				            		zk.getChildren(path+"/"+str, this);	
+				            		
+				            		
+				            		
+				            	}
+			            		
+			            	}
+			            		
+		            	}
+		            	
+		            	
+		            	//Add watcher to current path
+		            	if(!path.isEmpty()){
+		            		zk.getChildren(path, this);
+		            	}
+		            	
+				        }catch(Exception e){
+				        	
+				        	System.out.println(e.getMessage());
+				        	
+				        }
+	        	}
+	        	else{
+	        		flagDelete=false;
+	        	
+	        	}
+       		 
+		    }
+	        
+	        else if (event.getType() == Event.EventType.NodeDeleted){
+		        System.out.println("Node Delete Event");
+		        
+		        
+		        
+		        flagDelete=true;
+		
+	 		 
+		    }
+	        
+	        else if (event.getType() == Event.EventType.NodeCreated){
+	        System.out.println("Node Created Event"); 
+	        		 
+	        }
+	        
+	        else if (event.getType() == Event.EventType.NodeDataChanged){
+		        System.out.println("Node Data Changed Event");
+       		 
+		    }
+	       
+	        if (path != null) {	
+	            	try{
+	            	zk.getChildren(rootPath,this);
+	                }catch(Exception e){
+	                	System.out.println(e.getMessage());
+	                	
+	                }
+	            	
+	        }
+	    	
+	         
+	    }
+
+
+	    
+	    public static void main(String[] args){
+	    	
+	    	DaemonProcess dp = new DaemonProcess();
+	    	try{
+	    	Thread.sleep(1000000);
+	    	}catch (Exception e){
+	    		System.out.println("message"+e.getMessage()); //Catch error message
+	    	}
+	    	
+	    
+	    }
+	
+	    public static void deleteOneNode(){
+	    	
+	    	try{
+	    		
+	    		ZKDelete zkObj = new ZKDelete();
+	    		
+	    		for(int i=4;i<100;i++){
+	    			
+	    		zkObj.delete("/zkOperations"+i);
+	    		}
+	    		
+	    	}catch(Exception e){
+	    		
+	    		
+	    		
+	    	}
+	    	
+	    }
+	 
+	   
+
+	    public static void createNewGraph(int numofNodes){
+	
+	
+		MainGraph MG=new MainGraph();
+
+		String path; // Assign path to znode 
+	 	byte[] data; // Declare data
+   
+		      try {
+		
+		    	 ZKCreate ZKObj = new ZKCreate();
+		       
+		         for(int i=0;i<numofNodes;i++){
+		        	 
+		        	 path="/zkOperations"+Integer.toString(i);
+		        	 data="My zookeeper node".getBytes();
+		        	 
+		        	
+		        	 ZKObj.create(path, data); 
+		        	 
+		        	 System.out.println("Create "+i+" node");
+		        	 	        	 
+		        	 MG.addNode(path, data.toString());
+		         }
+		         
+		         
+		         MG.print_graph();
+		         
+		      } catch (Exception e) {
+			         System.out.println("message"+e.getMessage()); //Catch error message
+			         System.out.println("message"+e.toString()); //Catch error message
+		      }
+			
+	    }
+
+}
